3ab62d7f714011e43644714681831a02667a4720
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-03-08 12:34:20 +0100

    Retrieval works, trying writing

---
+++ fbpost.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from pyfb import Pyfb
+
+#Your APP ID. It Needs to register your application on facebook
+#http://developers.facebook.com/
+FACEBOOK_APP_ID = '305584816233697'
+
+facebook = Pyfb(FACEBOOK_APP_ID)
+
+#Opens a new browser tab instance and authenticates with the facebook API
+#It redirects to an url like http://www.facebook.com/connect/login_success.html#access_token=[access_token]&expires_in=0
+facebook.authenticate()
+
+#Copy the [access_token] and enter it below
+token = raw_input("Enter the access_token\n")
+
+#Sets the authentication token
+facebook.set_access_token(token)
+
+#Gets info about myself
+me = facebook.get_myself()
+
+print "-" * 40
+print "Name: %s" % me.name
+print "From: %s" % me.hometown.name
+print
+
+print "Speaks:"
+for language in me.languages:
+    print "- %s" % language.name
+
+print
+print "Worked at:"
+for work in me.work:
+    print "- %s" % work.employer.name
+
+print "-" * 40
+
+msg = raw_input("Enter message to post\n")
+
+facebook.comment(msg)
